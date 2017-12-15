2139272c9c3682d32f6e15b03bca7a99a6593418
Darryl <trigramthree@gmail.com>
2017-12-15 17:02:29 -0500

    HEROKU-CONFIG

--- src/app/components/components/home2/home2.component.ts
+++ src/app/components/components/home2/home2.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  templateUrl: './home2.component.html',
+  styleUrls: ['./home2.component.css']
 })
 export class HomeComponent2 implements OnInit {
   name:string;
