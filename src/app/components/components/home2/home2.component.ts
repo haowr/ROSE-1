695c9773019e0086a59021030c56f5b6bfb13572
Darryl <trigramthree@gmail.com>
2017-12-16 02:42:02 -0500

    ADD-NAVBAR-SELECTOR-TO-APP-COMPONENT

--- src/app/components/components/home2/home2.component.ts
+++ src/app/components/components/home2/home2.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-home2',
   templateUrl: './home2.component.html',
   styleUrls: ['./home2.component.css']
 })
