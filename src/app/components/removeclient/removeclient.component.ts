08ad27c2fb46f1c35bbf5e976baea150663042c6
Darryl <trigramthree@gmail.com>
2017-12-18 18:00:32 -0500

    'WORK-ON-REMOVE-CLIENT-COMPONENT

--- src/app/components/removeclient/removeclient.component.ts
+++ src/app/components/removeclient/removeclient.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../services/data.service';
-import { ClientService } from '../services/client.service';
+import { DataService } from '../../services/data.service';
+import { ClientService } from '../../services/client.service';
 
 @Component({
   selector: 'app-removeclient',
