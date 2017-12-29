a56ab6b84ca5e7d5a299fd4627b1510da1f75256
ohrha <trigramthree@gmail.com>
2017-12-28 20:27:55 -0500

    WORK-ON-EXPENDITURE-HISTORY

--- src/app/components/location/location.component.ts
+++ src/app/components/location/location.component.ts
@@ -283,19 +283,14 @@ export class LocationComponent implements OnInit {
                     let reducer = (accumulator, currentValue) => accumulator + currentValue;
                     this.totalCostThisMonth = totalCostThisMonthArray.reduce(reducer);
                     this.curexp = this.totalCostThisMonth;
-                   /* this.chartData = [
+                    this.chartData = [
 
                       { data: [this.curexp, this.expjan, this.expfeb, this.expmar, this.expapr, this.expmay, this.expjun, this.expjul, this.expaug, this.expsept, this.expoct, this.expnov, this.expdec], label: "Expenditures/Month" },
                       { data: [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], label: "$" },
 
 
-                      //{ data: [this.ordered, 0, 0, 0], label: this.location }
-
-
                     ];
-                    console.log(this.chartData[0]["data"]);
-                    this.chartLabels = ['Current', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-*/
+              
                     console.log(this.totalCostThisMonth);
 
                   }
@@ -337,7 +332,6 @@ export class LocationComponent implements OnInit {
                   let reducer = (accumulator, currentValue) => accumulator + currentValue;
                     this.totalCostThisMonth = totalCostThisMonthArray.reduce(reducer);
                     this.curexp = this.totalCostThisMonth;
-                    this.singleSubContractorsArray[i].expdec = this.totalCostThisMonth
 
                     this.chartData = [
 
