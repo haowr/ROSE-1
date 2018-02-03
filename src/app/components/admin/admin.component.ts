4fc419be10eae21dfe0f7a8dc6281842750b1d58
ohrha <trigramthree@gmail.com>
2018-02-03 13:40:23 -0500

    'ADD-REMOVE-CLIENT-FUNCTIONALITY

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -7,12 +7,13 @@ import { AuthService } from '../../services/auth.service';
 })
 export class AdminComponent implements OnInit {
   usersArray: object[] = [];
+  editLoading: boolean = false;
   arrayOfAdmins = [
 
-    {_id:""}
+    { _id: "" }
   ];
   thereMustBeOneAdmin: boolean;
-  cannotRemoveAdmin:boolean = false;
+  cannotRemoveAdmin: boolean = false;
   constructor(private authservice: AuthService) { }
 
   ngOnInit() {
@@ -38,7 +39,7 @@ export class AdminComponent implements OnInit {
 
   }
 
-  removeUser(currenttype, type, id){
+  removeUser(currenttype, type, id) {
 
 
     let usertype = {
@@ -47,13 +48,13 @@ export class AdminComponent implements OnInit {
       id: id
 
     }
-    if(currenttype !== "Admin"){
+    if (currenttype !== "Admin") {
+      this.editLoading = true;
+      this.authservice.removeUser(usertype).subscribe(data => {
+        this.editLoading = false;
 
-      this.authservice.removeUser(usertype).subscribe(data=>{
-
-
-          console.log(data)
-                  this.authservice.getUsers().subscribe(data => {
+        console.log(data)
+        this.authservice.getUsers().subscribe(data => {
           this.arrayOfAdmins = [];
 
           console.log(data)
@@ -74,19 +75,19 @@ export class AdminComponent implements OnInit {
 
       })
 
-    }else{
+    } else {
 
       this.cannotRemoveAdmin = true;
-setTimeout(()=>{
+      setTimeout(() => {
 
-    this.cannotRemoveAdmin = false;
-},4000)
+        this.cannotRemoveAdmin = false;
+      }, 4000)
     }
 
 
   }
   editUserType(currenttype, type, id) {
-//this.arrayOfAdmins = [];
+    //this.arrayOfAdmins = [];
     console.log(type)
     console.log(id)
     console.log(this.arrayOfAdmins.length)
@@ -99,9 +100,11 @@ setTimeout(()=>{
     if (type == "User" && this.arrayOfAdmins.length > 1) {
       console.log(this.arrayOfAdmins.length);
       console.log("fcondition")
+      this.editLoading = true;
       this.authservice.editUserType(usertype).subscribe(data => {
 
         console.log(data);
+        this.editLoading = false;
         this.authservice.getUsers().subscribe(data => {
           this.arrayOfAdmins = [];
 
@@ -123,27 +126,28 @@ setTimeout(()=>{
 
       })
 
-    }    else{
-    if(this.arrayOfAdmins[0]._id == id){
+    } else {
+      if (this.arrayOfAdmins[0]._id == id) {
 
 
-    this.thereMustBeOneAdmin =true;
-        setTimeout(()=>{
-  
-  
-          this.thereMustBeOneAdmin=false;
-        },2000)
-    }
-    
-  
-  
+        this.thereMustBeOneAdmin = true;
+        setTimeout(() => {
+
+
+          this.thereMustBeOneAdmin = false;
+        }, 2000)
       }
+
+
+
+    }
     if (type == "Admin" && this.arrayOfAdmins.length >= 0) {
 
-
+      this.editLoading = true;
       this.authservice.editUserType(usertype).subscribe(data => {
 
         console.log(data);
+        this.editLoading = false;
         this.authservice.getUsers().subscribe(data => {
           this.arrayOfAdmins = [];
 
@@ -165,7 +169,7 @@ setTimeout(()=>{
       })
     }
 
-  
+
   }
 
 }
